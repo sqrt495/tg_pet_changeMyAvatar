9d98722cfc8dd867bf058bcce3c73fa74cc7eb09
sqrt495 <ks.post.495@gmail.com>
2023-05-02 21:57:32 +0300

    Work with opencv

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from config import *
 import os
 import random
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 # client = TelegramClient('first_session', api_id, api_hash)
 # client.start()
 
+# work with path
 cwd = os.getcwd()
 print(os.listdir(cwd+'/covers'))
 
@@ -15,4 +18,22 @@ if get_random_covers_dir == '.DS_Store' or 'hoaqin_tears_video':
        get_random_covers_dir = random.choice(os.listdir(cwd+'/covers'))
 
 get_random_cover = random.choice(os.listdir(cwd+'/covers'+f'/{get_random_covers_dir}'))
-print(get_random_cover)
+
+# read img
+target_img = cv2.imread(cwd+'/covers/'+get_random_covers_dir+'/'+get_random_cover)
+height, width, channels = target_img.shape
+
+# check img center
+true_height, true_width = False, False
+if height > width:
+       true_height == width
+elif height < width:
+       true_width = height
+else: pass
+
+#  crop
+
+print(get_random_covers_dir,
+      get_random_cover,
+      height, width, channels,
+      sep='\n')
