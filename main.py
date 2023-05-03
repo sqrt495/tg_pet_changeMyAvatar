dd599994d3fd48a0500cdaf4be2fccd797b006ad
sqrt495 <ks.post.495@gmail.com>
2023-05-03 22:37:28 +0300

    Up main file

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ import numpy as np
 # work with path
 cwd = os.getcwd()
 print(os.listdir(cwd+'/covers'))
-
-get_random_covers_dir = random.choice(os.listdir(cwd+'/covers'))
+dirs_list = os.listdir(cwd+'/covers')
+get_random_covers_dir = random.choice(dirs_list)
 if get_random_covers_dir == '.DS_Store' or 'hoaqin_tears_video':
        get_random_covers_dir = random.choice(os.listdir(cwd+'/covers'))
 
-get_random_cover = random.choice(os.listdir(cwd+'/covers'+f'/{get_random_covers_dir}'))
+get_random_cover = random.choice(os.listdir(cwd+'/covers/'+get_random_covers_dir))
 
 # read img
 target_img = cv2.imread(cwd+'/covers/'+get_random_covers_dir+'/'+get_random_cover)
@@ -26,14 +26,17 @@ height, width, channels = target_img.shape
 # check img center
 true_height, true_width = False, False
 if height > width:
-       true_height == width
+    true_height = width
+    true_width = width
 elif height < width:
-       true_width = height
-else: pass
+    true_height = height
+    true_width = height
+else: true_height, true_width = height, width
 
 #  crop
 
-print(get_random_covers_dir,
-      get_random_cover,
-      height, width, channels,
+print(f'dir={get_random_covers_dir}',
+      f'img={get_random_cover}',
+      f'height={height}, width={width}, channels={channels}',
+      f'true_height={true_height}, true_width={true_width}',
       sep='\n')
