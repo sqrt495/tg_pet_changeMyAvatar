f642440bfd97c33275e2032c5fa7edcc1002c4ba
sqrt495 <ks.post.495@gmail.com>
2023-05-08 13:44:57 +0300

    up main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from telethon import TelegramClient, sync
+from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
 from config import *
 import os
 import random
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-# client = TelegramClient('first_session', api_id, api_hash)
-# client.start()
+
+client = TelegramClient('first_session', api_id, api_hash)
+client.start()
 
 # work with path
 cwd = os.getcwd()
@@ -23,20 +26,31 @@ get_random_cover = random.choice(os.listdir(cwd+'/covers/'+get_random_covers_dir
 target_img = cv2.imread(cwd+'/covers/'+get_random_covers_dir+'/'+get_random_cover)
 height, width, channels = target_img.shape
 
-# check img center
-true_height, true_width = False, False
-if height > width:
-    true_height = width
-    true_width = width
-elif height < width:
-    true_height = height
-    true_width = height
-else: true_height, true_width = height, width
-
-#  crop
+#Getting the bigger side of the image
+s = max(target_img.shape[0:2])
+#Creating a dark square with NUMPY
+img_font = np.zeros((s,s,3),np.uint8)
+#Getting the centering position
+ax,ay = (s - target_img.shape[1])//2,(s - target_img.shape[0])//2
+#Pasting the 'image' in a centering position
+img_font[ay:target_img.shape[0]+ay,ax:ax+target_img.shape[1]] = target_img
+# resize to 500x500
+resized_img = cv2.resize(target_img,
+                         dsize=(500, 500))
+#Saving the image
+path_to_save_resized_img = cwd + '/covers/' + get_random_covers_dir + '/' + get_random_cover + '_croped.jpg'
+cv2.imwrite(path_to_save_resized_img,
+            resized_img)
 
 print(f'dir={get_random_covers_dir}',
       f'img={get_random_cover}',
-      f'height={height}, width={width}, channels={channels}',
-      f'true_height={true_height}, true_width={true_width}',
+      f'height={height}, width={width}',
       sep='\n')
+
+print(path_to_save_resized_img)
+
+time.sleep(3)
+
+client(DeletePhotosRequest(client.get_profile_photos('me')))
+file = client.upload_file(str(path_to_save_resized_img))
+client(UploadProfilePhotoRequest(file))
